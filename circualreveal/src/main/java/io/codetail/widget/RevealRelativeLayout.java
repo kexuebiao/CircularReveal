f706715ba7602a93fecd37151f70919c7ce37f8f
sunny <87704470@qq.com>
2017-05-27 16:18:39 +0800

    “”

---
+++ circualreveal/src/main/java/io/codetail/widget/RevealRelativeLayout.java
@@ -0,0 +1,53 @@
+package io.codetail.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import io.codetail.animation.RevealViewGroup;
+import io.codetail.animation.ViewRevealManager;
+
+public class RevealRelativeLayout extends RelativeLayout implements RevealViewGroup {
+    private ViewRevealManager manager;
+
+    public RevealRelativeLayout(Context context) {
+        this(context, null);
+    }
+
+    public RevealRelativeLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RevealRelativeLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs);
+        manager = new ViewRevealManager();
+    }
+
+    @Override
+    protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
+        try {
+            canvas.save();
+
+            manager.transform(canvas, child);
+            return super.drawChild(canvas, child, drawingTime);
+        } finally {
+            canvas.restore();
+        }
+    }
+
+    @Override
+    public ViewRevealManager getViewRevealManager() {
+        return manager;
+    }
+
+    @Override
+    public void setViewRevealManager(ViewRevealManager manager) {
+        if (manager == null) {
+            throw new NullPointerException("ViewRevealManager is null");
+        }
+
+        this.manager = manager;
+    }
+}
